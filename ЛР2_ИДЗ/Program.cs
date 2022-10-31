14e5785cedbfe71677465c98b7301d59ba8a52fd
yaolg <yaolg@DESKTOP-PS4EPC5>
2022-10-31 20:48:46 +0300

    Добавьте файлы проекта.

---
+++ ЛР2_ИДЗ/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР2_ИДЗ
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            Sklad.Initial();
+            Console.WriteLine("Склад готов к работе");
+            Console.Write("Как к вам обращаться? ");
+            string fio = Console.ReadLine();
+            Console.WriteLine("Уважаемый " + fio + ", выберите действие\n" +
+                "1-Вывод списка товаров\n" +
+                "2-Добавть товар\n" +
+                "0-Выход");
+            int doIt = int.MaxValue;
+            while (doIt > 0)
+            {
+                Console.Write("Ваш выбор: ");
+                doIt = int.Parse(Console.ReadLine());
+                switch (doIt)
+                {
+                    case 1:
+                        Sklad.GetList();
+                        break;
+                    case 2:
+                        Sklad.Add();
+                        break;
+                    case 0:
+                        doIt = 0;
+                        break;
+                    default: break;
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ЛР2_ИДЗ/Sklad.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР2_ИДЗ
+{
+    internal class Sklad
+    {
+        public static List<Product> products = new List<Product>();
+
+        public static void Initial()
+        {
+            var product=new Product();
+            product.price = 500;
+            product.name = "Mango";
+            products.Add(product);
+            product = new Product();
+            product.price = 1300;
+            product.name = "t-shirt";
+            products.Add(product);
+        }
+
+        public static void Add()
+        {
+            var product=new Product();
+            Console.WriteLine("Введите название товара:");
+            product.name = Console.ReadLine();
+            Console.WriteLine("Введите цену товара:");
+            product.price = int.Parse(Console.ReadLine());
+            products.Add(product);
+            Console.WriteLine("Товар успешно добавлен");
+        }
+
+        public static void GetList()
+        {
+            if (products.Count != 0)
+            {
+                foreach (Product tariff in products)
+                {
+                    Console.WriteLine(tariff.price + "\n" + tariff.name );
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Список продуктов пуст");
+            }
+        }
+    }
+}
